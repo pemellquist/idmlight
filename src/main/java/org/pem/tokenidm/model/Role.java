3d2cb13c240f85ceed4b4fa97ab9cbc6b0e04cd7
Peter Mellquist <pemellquist@gmail.com>
2014-06-10 13:30:55 -0700

    new

---
+++ src/main/java/org/pem/tokenidm/model/Role.java
@@ -0,0 +1,36 @@
+package org.pem.tokenidm.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "role")
+public class Role {
+   private Integer id;
+   private String name;
+   private String description;
+
+   public Integer getId() {
+      return id;
+   }
+
+   public void setId(Integer id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }  
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+
+}
+
