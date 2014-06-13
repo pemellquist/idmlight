f715b93a81f18b4cf41f4d52996e027295c1e2d0
Peter Mellquist <pemellquist@gmail.com>
2014-06-12 18:26:06 -0700

    refactor

--- tests/test.sh
+++ tests/test.sh
@@ -25,7 +25,6 @@ echo '['$TESTCOUNT']' $NAME
 STATUS=$(curl -X DELETE -k -s -H Accept:application/json -o result.json -w '%{http_code}' $URL)
 if [ $STATUS -eq $PASSCODE ]; then
    ((PASSCOUNT++))
-   cat result.json | python -mjson.tool
    echo "[PASS]"
 else
    echo "[FAIL] Status=" $STATUS
@@ -70,42 +69,53 @@ echo
 # 
 
 NAME="get all tenants"
-URL="https://$TARGET/V1/tenants"
+URL="https://$TARGET/v1/tenants"
 PASSCODE=200
 getit
 
 NAME="create a new tenant"
-URL="https://$TARGET/V1/tenants"
+URL="https://$TARGET/v1/tenants"
 POSTFILE=tenant.json
 PASSCODE=201
 postit
 
-NAME="get tenant"
-URL="https://$TARGET/V1/tenants/1"
+NAME="get tenant 1"
+URL="https://$TARGET/v1/tenants/1"
 PASSCODE=200
 getit
 
-NAME="delete tenant"
-URL="https://$TARGET/V1/tenants/1"
+NAME="delete tenant 1"
+URL="https://$TARGET/v1/tenants/1"
 PASSCODE=204
 deleteit
 
-NAME="create a new tenants"
-URL="https://$TARGET/V1/tenants"
+NAME="create a new tenant"
+URL="https://$TARGET/v1/tenants"
 POSTFILE=tenant.json
 PASSCODE=201
 postit
 
-NAME="get tenants"
-URL="https://$TARGET/V1/tenants"
+NAME="get all tenants"
+URL="https://$TARGET/v1/tenants"
 PASSCODE=200
 getit
 
-NAME="update a tenant"
-URL="https://$TARGET/V1/tenants/2"
+NAME="update tenant 2"
+URL="https://$TARGET/v1/tenants/2"
 PUTFILE=tenant.json
 PASSCODE=200
 putit
+
+NAME="create a new tenant"
+URL="https://$TARGET/v1/tenants"
+POSTFILE=tenant.json
+PASSCODE=201
+postit
+
+NAME="get all tenants"
+URL="https://$TARGET/v1/tenants"
+PASSCODE=200
+getit
 
 
 #
@@ -113,93 +123,145 @@ putit
 #
 
 NAME="get all users"
-URL="https://$TARGET/V1/users"
+URL="https://$TARGET/v1/users"
 PASSCODE=200
 getit
 
 NAME="create a new user"
-URL="https://$TARGET/V1/users"
+URL="https://$TARGET/v1/users"
 POSTFILE=user.json
 PASSCODE=201
 postit
 
 NAME="get all users"
-URL="https://$TARGET/V1/users"
+URL="https://$TARGET/v1/users"
 PASSCODE=200
 getit
 
-NAME="get a user"
-URL="https://$TARGET/V1/users/1"
+NAME="get user 1"
+URL="https://$TARGET/v1/users/1"
 PASSCODE=200
 getit
 
-NAME="delete user"
-URL="https://$TARGET/V1/users/1"
+NAME="delete user 1"
+URL="https://$TARGET/v1/users/1"
 PASSCODE=204
 deleteit
 
 NAME="get all users"
-URL="https://$TARGET/V1/users"
+URL="https://$TARGET/v1/users"
 PASSCODE=200
 getit
 
 NAME="create a new user"
-URL="https://$TARGET/V1/users"
+URL="https://$TARGET/v1/users"
 POSTFILE=user.json
 PASSCODE=201
 postit
 
 NAME="update a user"
-URL="https://$TARGET/V1/users/2"
+URL="https://$TARGET/v1/users/2"
 PUTFILE=user.json
 PASSCODE=200
 putit
 
+NAME="create a new user"
+URL="https://$TARGET/v1/users"
+POSTFILE=user.json
+PASSCODE=201
+postit
+
+NAME="get all users"
+URL="https://$TARGET/v1/users"
+PASSCODE=200
+getit
 
 # ROLE TESTS
 
 NAME="get all roles"
-URL="https://$TARGET/V1/roles"
+URL="https://$TARGET/v1/roles"
 PASSCODE=200
 getit
 
 NAME="create a new role"
-URL="https://$TARGET/V1/roles"
+URL="https://$TARGET/v1/roles"
 POSTFILE=role.json
 PASSCODE=201
 postit
 
 NAME="get all roles"
-URL="https://$TARGET/V1/roles"
+URL="https://$TARGET/v1/roles"
 PASSCODE=200
 getit
 
-NAME="get a role"
-URL="https://$TARGET/V1/roles/1"
+NAME="get role 1"
+URL="https://$TARGET/v1/roles/1"
 PASSCODE=200
 getit
 
-NAME="delete role"
-URL="https://$TARGET/V1/roles/1"
+NAME="delete role 1"
+URL="https://$TARGET/v1/roles/1"
 PASSCODE=204
 deleteit
 
 NAME="create a new role"
-URL="https://$TARGET/V1/roles"
+URL="https://$TARGET/v1/roles"
 POSTFILE=role.json
 PASSCODE=201
 postit
 
-NAME="update a role"
-URL="https://$TARGET/V1/roles/2"
+NAME="update role 2"
+URL="https://$TARGET/v1/roles/2"
 PUTFILE=role-put.json
 PASSCODE=200
 putit
 
+NAME="create a new role"
+URL="https://$TARGET/v1/roles"
+POSTFILE=role.json
+PASSCODE=201
+postit
+
+
 NAME="get all roles"
-URL="https://$TARGET/V1/roles"
+URL="https://$TARGET/v1/roles"
 PASSCODE=200
 getit
+
+# TUR tests
+
+NAME="grant a role"
+URL="https://$TARGET/v1/tenants/2/users/2/roles"
+POSTFILE=grant.json
+PASSCODE=201
+postit
+
+URL="https://$TARGET/v1/tenants/2/users/2/roles"
+POSTFILE=grant.json
+PASSCODE=403
+postit
+
+
+NAME="get all roles for tenant and user"
+URL="https://$TARGET/v1/tenants/2/users/2/roles"
+PASSCODE=200
+getit
+
+NAME="delete a grant"
+URL="https://$TARGET/v1/tenants/2/users/2/roles/2"
+PASSCODE=204
+deleteit
+
+NAME="delete a grant"
+URL="https://$TARGET/v1/tenants/2/users/2/roles/2"
+PASSCODE=404
+deleteit
+
+NAME="get all roles for tenant and user"
+URL="https://$TARGET/v1/tenants/2/users/2/roles"
+PASSCODE=200
+getit
+
 
 #
 # RESULTS
