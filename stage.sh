51c51ebcab2ca0f673dd644e6fb30fcce075d26b
Peter Mellquist <pemellquist@gmail.com>
2014-06-13 19:18:52 -0700

    openidm

--- stage.sh
+++ stage.sh
@@ -1,4 +1,4 @@
-sudo service tokenidm stop
-sudo dpkg -i tokenidm-1.00/tokenidm-1.00.deb
-sudo service tokenidm start
+sudo service openidm stop
+sudo dpkg -i openidm-1.00/openidm-1.00.deb
+sudo service openidm start
 
