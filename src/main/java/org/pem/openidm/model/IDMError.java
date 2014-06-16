ba467a0650d98eafaabdc6fc5a147789fa7ae6a8
Peter Mellquist <pemellquist@gmail.com>
2014-06-16 14:28:29 -0700

    new

---
+++ src/main/java/org/pem/openidm/model/IDMError.java
@@ -0,0 +1,29 @@
+package org.pem.openidm.model;
+
+import org.apache.log4j.Logger;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "idmerror")
+public class IDMError{
+   private static Logger logger = Logger.getLogger(IDMError.class);
+
+   private String message;
+   private String details;
+
+   public String getMessage() {
+      return message;
+   }
+
+   public void setMessage(String msg) {
+      this.message = msg;
+   }
+
+   public String getDetails() {
+      return details;
+   }
+
+   public void setDetails(String details) {
+      this.details = details;
+   }
+	
+}
