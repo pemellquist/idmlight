370a2030d90658a63ca6ab464dc34fdbaaaa38ac
Peter Mellquist <pemellquist@gmail.com>
2014-06-17 12:03:49 -0700

    user pwd submit

---
+++ src/main/java/org/pem/openidm/model/UserPwd.java
@@ -0,0 +1,27 @@
+package org.pem.openidm.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "userpwd")
+public class UserPwd {
+   private String username;
+   private String userpwd;
+
+   public String getUsername() {
+      return username;
+   }
+
+   public void setUsername(String name) {
+      this.username = name;
+   }  
+
+   public String getUserpwd() {
+      return userpwd;
+   }
+
+   public void setUserpwd(String pwd) {
+      this.userpwd = pwd;
+   }
+
+}
+
